f011f7287a5d753788b4f9eea502638384e31dc6
Johnathan Yu <johnathanzyu@gmail.com>
2019-10-26 17:06:14 -0400

    did stuff

--- app/src/main/java/com/example/myapplication/ShopActivity.java
+++ app/src/main/java/com/example/myapplication/ShopActivity.java
@@ -1,50 +1,40 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.text.method.ScrollingMovementMethod;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.LinkedList;
 
-public class MainActivity extends AppCompatActivity {
+public class ShopActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_shop);
 
         final EditText textField = findViewById(R.id.textField);
 
         final LinearLayout shoppingList = findViewById(R.id.shoppinglist);
 
-        final Button shopButton = findViewById(R.id.shopButton);
+
 
         textField.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View view, int keyCode, KeyEvent event) {
                 if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
-                    shoppingList.addView(new ListEntry(MainActivity.this, textField.getText().toString()));
+                    shoppingList.addView(new ListEntry(ShopActivity.this, textField.getText().toString()));
 
                     return true;
                 }
                 return false;
             }
-        });
-
-        shopButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), ShopActivity.class);
-                startActivityForResult(myIntent, 0);
-            }
-
         });
     }
 }
